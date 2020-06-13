780ca4c8baa8899e98deae764cdc7ba98ad95369
evansinho <igiri.evanson@gmail.com>
2020-06-13 11:02:33 +0100

    made changes with respect to the review given

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,10 +1,6 @@
 class FriendshipsController < ApplicationController
   def create
-    @friendship = Friendship.new
-    @friendship.sender_id = current_user.id
-    @friendship.receiver_id = params[:friend_id]
-    @friendship.status = 'pending'
-    @friendship.save
+    send_request
     flash[:notice] = 'Request sent successfully'
     redirect_to users_path
   end
@@ -21,5 +17,15 @@ class FriendshipsController < ApplicationController
     Friendship.create(sender_id: current_user.id,
                       receiver_id: params[:sender_id], status: 'accepted')
     redirect_to received_requests_path
+  end
+
+  private
+  
+  def send_request
+    @friendship = Friendship.new
+    @friendship.sender_id = current_user.id
+    @friendship.receiver_id = params[:friend_id]
+    @friendship.status = 'pending'
+    @friendship.save
   end
 end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -11,6 +11,29 @@ module FriendshipsHelper
     User.find_by(id: user_id)
   end
 
+  def timeline_posts
+    arr = []
+    associated_posts = []
+    @friends = Friendship.where('(sender_id = ? and status = ?) OR (receiver_id = ? and status = ?)',
+                                current_user.id, 'accepted', current_user.id, 'accepted')
+    @friends.each do |friend|
+      if friend.sender_id == current_user.id
+        arr.push(friend.receiver_id)
+      else
+        arr.push(friend.sender_id)
+      end
+    end
+    arr.push(current_user.id)
+
+    posts = Post.all
+
+    posts.each do |post|
+      associated_posts.push(post) if arr.include?(post.user_id)
+    end
+
+    associated_posts.reverse
+  end
+
   def pending_requests
     Friendship.where('receiver_id = ? and status = ?', current_user.id, 'pending').count
   end
